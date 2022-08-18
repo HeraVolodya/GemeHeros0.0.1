b1b46eacbc7ab049259a6aedfd76204c418afe60
Volodymyr <heravolodymyr1991@gmail.com>
2022-08-19 00:31:38 +0300

    creating a random number generator

--- Heros0.0.1/Heros0.0.1/Program.cs
+++ Heros0.0.1/Heros0.0.1/Program.cs
@@ -10,6 +10,8 @@ namespace Heros0._0._1
     {
         static void Main(string[] args)
         {
+            SuperPowerGenerator superPowerGenerator = new SuperPowerGenerator();
+            Console.WriteLine(superPowerGenerator.Random());
         }
     }
 }
